610f4e39822dfec8d0b43e667ba23e118187b0db
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-08-06 18:28:48 +0300

    .

--- train.py
+++ train.py
@@ -105,6 +105,7 @@ def train_vib(model, dataset):
             if batch_idx % 50 == 0:
                 logger.info(f'batch_idx: {batch_idx}, loss: {round(running_loss/50, 4)}')
                 logger.info(f'supervised loss: {round(supervised_loss.item(), 4)}')
+                running_loss = 0
             batch_idx += 1
         acc_train = round((correct/(total+0.00001))*100, 2)
         logger.info(f'epoch: {epoch}, train accuracy: {acc_train}')
