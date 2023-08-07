48a454a079268bd84a70fa34e98e25f36d12693f
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-08-08 01:27:45 +0300

    .

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def train_vib(model, dataset):
         transforms.RandomHorizontalFlip(),
         transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
         transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.8, 1.2)),
-        # transforms.RandomPerspective(distortion_scale=0.5, p=0.5, interpolation=3),
+        transforms.RandomPerspective(distortion_scale=0.5, p=0.5, interpolation=3),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
