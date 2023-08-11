5ea04ee3800ef996047edea660654d6708bfee73
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-08-11 15:20:11 +0300

    .

--- nets.py
+++ nets.py
@@ -103,14 +103,6 @@ def ResNet18(e):
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
 
-import torch
-import torch.nn as nn
-import torchvision.models as models
-
-
-import torch
-import torch.nn as nn
-
 class VIB_Decoder(nn.Module):
     def __init__(self, latent_dim):
         super(VIB_Decoder, self).__init__()
@@ -119,25 +111,104 @@ class VIB_Decoder(nn.Module):
         self.setup = setup
 
         # Fully connected layer to map latent_dim to initial convolutional shape
-        self.fc = nn.Linear(latent_dim, 512 * 4 * 4)
+        self.fc = nn.Linear(latent_dim, 32 * 4 * 4)
 
         # Transposed convolutional layers
-        self.deconv1 = nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1)
-        self.deconv2 = nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1)
-        self.deconv3 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
-        self.deconv4 = nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1)
+        self.deconv1 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(32, 16, kernel_size=5, stride=2, padding=0)
+        self.deconv3 = nn.ConvTranspose2d(16, 8, kernel_size=5, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(8, 3, kernel_size=3, stride=1, padding=1)
 
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.fc(x)
-        x = x.view(-1, 512, 4, 4)  # Reshape the tensor
+        x = x.view(-1, 32, 4, 4)
         x = self.relu(self.deconv1(x))
         x = self.relu(self.deconv2(x))
         x = self.relu(self.deconv3(x))
         x = self.deconv4(x)
+        x = x[:, :, :32, :32]
         return x
+
+
+class ResizeConv2d(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, scale_factor, mode='nearest'):
+        super().__init__()
+        self.scale_factor = scale_factor
+        self.mode = mode
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=1)
+
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
+        x = self.conv(x)
+        return x
+
+class BasicBlockDec(nn.Module):
+
+    def __init__(self, in_planes, stride=1):
+        super().__init__()
+
+        planes = int(in_planes/stride)
+
+        self.conv2 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(in_planes)
+        # self.bn1 could have been placed here, but that messes up the order of the layers when printing the class
+
+        if stride == 1:
+            self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn1 = nn.BatchNorm2d(planes)
+            self.shortcut = nn.Sequential()
+        else:
+            self.conv1 = ResizeConv2d(in_planes, planes, kernel_size=3, scale_factor=stride)
+            self.bn1 = nn.BatchNorm2d(planes)
+            self.shortcut = nn.Sequential(
+                ResizeConv2d(in_planes, planes, kernel_size=3, scale_factor=stride),
+                nn.BatchNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = torch.relu(self.bn2(self.conv2(x)))
+        out = self.bn1(self.conv1(out))
+        out += self.shortcut(x)
+        out = torch.relu(out)
+        return out
+
+class ResNet18Dec(nn.Module):
+
+    def __init__(self, num_Blocks=[2,2,2,2], z_dim=setup['latent_dim'], nc=3):
+        super().__init__()
+        self.in_planes = 512
+
+        self.linear = nn.Linear(z_dim, 512)
+
+        self.layer4 = self._make_layer(BasicBlockDec, 256, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlockDec, 128, 2, stride=2)
+        self.layer2 = self._make_layer(BasicBlockDec, 64, 2, stride=2)
+        self.layer1 = self._make_layer(BasicBlockDec, 64, 2, stride=1)
+        self.conv1 = ResizeConv2d(64, nc, kernel_size=3, scale_factor=1)
+
+    def _make_layer(self, BasicBlockDec, planes, num_Blocks, stride):
+        strides = [stride] + [1]*(num_Blocks-1)
+        layers = []
+        for stride in reversed(strides):
+            layers += [BasicBlockDec(self.in_planes, stride)]
+        self.in_planes = planes
+        return nn.Sequential(*layers)
+
+    def forward(self, z):
+        x = self.linear(z)
+        x = x.view(z.size(0), 512, 1, 1)
+        x = F.interpolate(x, scale_factor=4)
+        x = self.layer4(x)
+        x = self.layer3(x)
+        x = self.layer2(x)
+        x = self.layer1(x)
+        x = self.conv1(x)
+        return x
+
 
 
 
@@ -146,7 +217,8 @@ class VIBNet(nn.Module):
         super(VIBNet, self).__init__()
         torch.manual_seed(seed)
         self.encoder = ResNet18(seed)
-        self.decoder = VIB_Decoder(setup['latent_dim'])
+        # self.decoder = VIB_Decoder(setup['latent_dim'])
+        self.decoder = ResNet18Dec(setup['latent_dim'])
 
         self.fc_combined = nn.Linear(512, latent_dim * 2)
         self.fc_classifier = nn.Linear(latent_dim, num_classes)
