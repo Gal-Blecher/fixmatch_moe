9ca6888211c5147a754fe893708cfc2b85fa2004
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-09-13 12:46:27 +0300

    .

--- main.py
+++ main.py
@@ -6,10 +6,25 @@ import build
 import os
 import nets
 # from torchsummary import summary
+import subprocess
 
 torch.manual_seed(42)
 
 if __name__ == '__main__':
+    def get_git_info():
+        try:
+            branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stderr=subprocess.STDOUT,
+                                             text=True).strip()
+            commit = subprocess.check_output(['git', 'rev-parse', 'HEAD'], stderr=subprocess.STDOUT, text=True).strip()
+            return branch, commit
+        except subprocess.CalledProcessError as e:
+            # Handle any errors that occur when running Git commands
+            return None, None
+
+    branch, commit = get_git_info()
+    setup['commit'] = commit
+    setup['branch'] = branch
+
     path = './models/' + setup['experiment_name']
     if not os.path.exists(path):
         os.makedirs(path)
