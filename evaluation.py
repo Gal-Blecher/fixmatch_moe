0dcc783e6b3614174deb9fa7053a41794b590ee8
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-09-14 18:53:54 +0300

    .

--- evaluation.py
+++ evaluation.py
@@ -8,7 +8,6 @@ import datasets
 import build
 from config import setup
 import nets
-from train import vib_test, moe_test
 from sklearn.metrics import classification_report
 
 def load_model(setup_dict):
@@ -116,16 +115,47 @@ def moe_expert_eval(test_loader, expert):
         acc = round((correct / total)*100, 2)
         return acc, predicted_list, target_list
 
+def experts_dominanace(model, loader):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.eval()
+    model.to(device)
+    dominant_dict = {}
+    for i, (images, labels) in enumerate(loader, start=1):
+      images = images.to(device)
+      labels = labels.to(device)
+      # outputs = model(images)[0].squeeze(1)
+      weights = model(images)[1].squeeze(1)
+      dominant_experts = torch.max(weights, axis=1)[1]
+      for l in torch.unique(labels):
+        dom_exp, count = torch.unique(dominant_experts[labels==l], return_counts=True)
+        for c, exp in enumerate(dom_exp):
+          if str(exp.item()) not in dominant_dict.keys():
+            dominant_dict[str(exp.item())] = torch.zeros(10).to(device)
+          dominant_dict[str(exp.item())][l] += count[c]
+    return dominant_dict
+
+def plot_exp_dist(dominant_dict):
+    fig, axes = plt.subplots(len(dominant_dict),1, sharex=False, sharey=False, figsize=(20,40))
+    for i,key in enumerate(dominant_dict.keys()):
+        tot = dominant_dict[key].sum().item()
+        print(f'expert {key} is the dominant expert for {tot} instances')
+        axes[i].bar(np.arange(10),dominant_dict[key].cpu())
+        axes[i].set_title(f'Expert {key}')
+    plt.show()
+
+
 
 if __name__ == '__main__':
     setup_dict = {
         'latent_dim': 256,
-        'load_path': '/Users/galblecher/Desktop/Thesis_out/vib_cifar/fixmatch/moe_vib_ssl_14/model.pkl'
+        'load_path': '/Users/galblecher/Desktop/Thesis_out/vib_cifar/fixmatch/moe_fm/moe_vib_ssl_52/model.pkl'
     }
     model = load_model(setup_dict)
     dataset = datasets.get_dataset()
-    acc, predicted_list, target_list = moe_expert_eval(dataset['test_loader'], model.expert4)
-    print(classification_report(target_list, predicted_list))
-    low_dim_data = two_dims_from_z(dataset, model.expert4)
+    # domininat_dict = experts_dominanace(model, dataset['test_loader'])
+    # plot_exp_dist(domininat_dict)
+    # acc, predicted_list, target_list = moe_expert_eval(dataset['test_loader'], model.expert3)
+    # print(classification_report(target_list, predicted_list))
+    low_dim_data = two_dims_from_z(dataset, model.expert3)
     plot_scatter_with_labels(low_dim_data)
     t=1
